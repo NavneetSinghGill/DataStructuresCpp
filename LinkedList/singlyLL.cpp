b6eb44361a31b8cdd6bc7b949ac5c95f64db839e
Navneet Singh Gill <navneet105gill@gmail.com>
2022-02-15 19:05:47 -0500

    SinglyLL: add delete last with double pointer to set head to NULL when no elements are present

--- LinkedList/singlyLL.cpp
+++ LinkedList/singlyLL.cpp
@@ -10,21 +10,26 @@ struct node {
 node* createLL(int data);
 void print(node *list);
 node* add(int data, node *head);
+void deleteLast(node **head);
 
 int main() {
     node *head = NULL;
 
     head = createLL(5);
-    add(7, head);
-    add(6, head);
-    add(4, head);
-
+    // add(7, head);
+    // add(6, head);
+    // add(4, head);
     print(head);
+    deleteLast(&head);
+    head = createLL(99);
+    print(head);
+
     return 0;
 }
 
 void print(node *list) {
     node *traversalPointer = list;
+
     while(traversalPointer != NULL) {
         cout << traversalPointer->data;
         if(traversalPointer->next != NULL) {
@@ -50,6 +55,7 @@ node* add(int data, node *head) {
     //head is NULL only when linked list is empty
     if(head == NULL) {
         head = newData;
+        cout << "Add " << data << " to newly created singly linked list" << endl;
         return head;
     }
 
@@ -58,5 +64,25 @@ node* add(int data, node *head) {
         head = head -> next;
     }
     head->next = newData;
+    cout << "Add " << data << " to singly linked list" << endl;
     return originalHead;
+}
+
+//Used double pointer to set head to NULL when no elements are present
+void deleteLast(node **head) {
+    if(*head != NULL) {
+        node* prev = NULL;
+        while((*head) -> next != NULL) {
+            prev = *head;
+            *head = (*head) -> next;
+        }
+        if(prev != NULL) {
+            prev->next = NULL;
+        }
+        if(*head != NULL) {
+            cout << "Delete " << (*head)->data << " from end of singly linked list" << endl;
+            free(*head);
+            *head = NULL;
+        }
+    }
 }
